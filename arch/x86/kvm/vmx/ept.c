be8ae61cd1cf085d4b0d7eaca21555dd67fb22b8
Sean Christopherson <sean.j.christopherson@intel.com>
2019-07-30 08:44:26 -0700

    KVM: VMX: Use "suppress #VE bit" to identify MMIO EPT entries
    Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>

--- arch/x86/kvm/vmx/ept.c
+++ arch/x86/kvm/vmx/ept.c
@@ -8,13 +8,14 @@ void ept_enable_tdp(void)
 	const u64 a_mask = enable_ept_ad_bits ? VMX_EPT_ACCESS_BIT : 0ull;
 	const u64 d_mask = enable_ept_ad_bits ? VMX_EPT_DIRTY_BIT : 0ull;
 	const u64 p_mask = (cpu_has_vmx_ept_execute_only() ? 0ull :
-			    VMX_EPT_READABLE_MASK);
+			    VMX_EPT_READABLE_MASK) | VMX_EPT_SUPPRESS_VE_BIT;
+
+	kvm_mmu_set_spte_init_value(VMX_EPT_SUPPRESS_VE_BIT);
 
 	kvm_mmu_set_mask_ptes(VMX_EPT_READABLE_MASK, a_mask, d_mask, 0ull,
 		VMX_EPT_EXECUTABLE_MASK, p_mask, VMX_EPT_RWX_MASK, 0ull);
 
-	kvm_mmu_set_mmio_spte_mask(VMX_EPT_RWX_MASK,
-				   VMX_EPT_MISCONFIG_WX_VALUE, 0);
+	kvm_mmu_set_mmio_spte_mask(VMX_EPT_SUPPRESS_VE_BIT, 0, 0);
 
 	kvm_enable_tdp();
 }
