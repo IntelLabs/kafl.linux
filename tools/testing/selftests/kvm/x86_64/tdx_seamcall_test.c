f9f76e09d6a56f755724c495f19eda9a4be4d18b
Sean Christopherson <sean.j.christopherson@intel.com>
2020-03-13 13:46:14 -0700

    KVM: selftests: Randomize unused SEAMCALL params in TDX-SEAM test
    Add a pseudo-rng framework to the SEAMCALL test and use it to randomize
    parameters that should be ignored by TDX-SEAM.
    
    Co-developed-by: Zhang Chen <chen.zhang@intel.com>
    Signed-off-by: Zhang Chen <chen.zhang@intel.com>
    Signed-off-by: Sean Christopherson <sean.j.christopherson@intel.com>

--- tools/testing/selftests/kvm/x86_64/tdx_seamcall_test.c
+++ tools/testing/selftests/kvm/x86_64/tdx_seamcall_test.c
@@ -36,6 +36,89 @@
 
 #include "../../../../../arch/x86/kvm/intel/tdx_arch.h"
 
+static inline u8 __rand_u8(u8 mask)
+{
+	return (u8)rand() & mask;
+}
+
+static inline u8 rand_u8(void)
+{
+	return __rand_u8(0xff);
+}
+
+static inline u16 __rand_u16(u16 mask)
+{
+	return (u16)rand() & mask;
+}
+
+static inline u16 rand_u16(void)
+{
+	return __rand_u16(0xffff);
+}
+
+static inline u32 __rand_u32(u32 mask)
+{
+	return (u32)rand() & mask;
+}
+
+static inline u32 rand_u32(void)
+{
+	return __rand_u32(-1u);
+}
+
+static inline u64 __rand_u64(u64 mask)
+{
+	return (u64)rand() & mask;
+}
+
+static inline u64 rand_u64(void)
+{
+	return __rand_u64(-1ull);
+}
+
+static unsigned int parse_seed(int argc, char **argv)
+{
+	unsigned int seed;
+	char *tmp = NULL;
+	int c;
+
+	c = getopt(argc, argv, "s:");
+	if (c == -1)
+		return 0;
+
+	TEST_ASSERT(c == 's', "Unknown option '%s'", c);
+
+	seed = (unsigned int)strtoul(optarg, &tmp, 0);
+	TEST_ASSERT(*tmp == '\0' && tmp != optarg,
+		    "Unabled to parse seed '%s'\n", optarg);
+
+	return seed;
+}
+
+static void init_random_seed(int argc, char **argv)
+{
+	unsigned int seed;
+	int fd, ret;
+
+	seed = parse_seed(argc, argv);
+	if (seed)
+		goto init_srand;
+
+	fd = open("/dev/urandom", O_RDONLY);
+	TEST_ASSERT(fd >= 0, "failed to open /dev/kvm, fd: %i errno: %i",
+		    fd, errno);
+
+	ret = read(fd, &seed, sizeof(seed));
+	TEST_ASSERT(ret == sizeof(seed),
+		    "failed read() on /dev/urandom, ret: %i errno: %i",
+		    ret, errno);
+	close(fd);
+
+init_srand:
+	printf("TDX random seed: %u\n", seed);
+	srand(seed);
+}
+
 static inline u64 seamcall(int fd, u64 rax, u64 rcx, u64 rdx, u64 r8, u64 r9,
 			   u64 r10)
 {
@@ -59,23 +142,23 @@ static inline u64 seamcall(int fd, u64 rax, u64 rcx, u64 rdx, u64 r8, u64 r9,
 
 static inline u64 seamcall5(int fd, u64 rax, u64 rcx, u64 rdx, u64 r8, u64 r9)
 {
-	return seamcall(fd, rax, rcx, rdx, r8, r9, 0);
+	return seamcall(fd, rax, rcx, rdx, r8, r9, rand_u64());
 }
 static inline u64 seamcall4(int fd, u64 rax, u64 rcx, u64 rdx, u64 r8)
 {
-	return seamcall5(fd, rax, rcx, rdx, r8, 0);
+	return seamcall5(fd, rax, rcx, rdx, r8, rand_u64());
 }
 static inline u64 seamcall3(int fd, u64 rax, u64 rcx, u64 rdx)
 {
-	return seamcall4(fd, rax, rcx, rdx, 0);
+	return seamcall4(fd, rax, rcx, rdx, rand_u64());
 }
 static inline u64 seamcall2(int fd, u64 rax, u64 rcx)
 {
-	return seamcall3(fd, rax, rcx, 0);
+	return seamcall3(fd, rax, rcx, rand_u64());
 }
 static inline u64 seamcall1(int fd, u64 rax)
 {
-	return seamcall2(fd, rax, 0);
+	return seamcall2(fd, rax, rand_u64());
 }
 
 int main(int argc, char **argv)
@@ -87,6 +170,8 @@ int main(int argc, char **argv)
 	fd = open(KVM_DEV_PATH, O_RDWR);
 	TEST_ASSERT(fd >= 0, "failed to open /dev/kvm fd: %i errno: %i",
 		    fd, errno);
+
+	init_random_seed(argc, argv);
 
 	/* Create a dummy VM to coerce KVM into doing VMXON. */
 	vm = vm_create_default(0, 0, NULL);
