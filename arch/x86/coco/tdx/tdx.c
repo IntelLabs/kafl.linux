240cb76414e32c2ac9f80f36be353597e3b8a6cc
Andi Kleen <ak@linux.intel.com>
2021-06-01 23:21:09 -0700

    x86/tdx: Enable virtio restricted memory access for TDX
    In virtio the host decides whether the guest uses the DMA
    API or not using the strangely named VIRTIO_F_ACCESS_PLATFORM
    bit (which really indicates whether the DMA API is used or not)
    
    For hardened virtio on TDX, it is expected that swiotlb is
    always used, which requires using the DMA API.  While IO wouldn't
    really work without the swiotlb, it might be possible that an
    attacker forces swiotlbless IO to manipulate memory in the guest.
    
    So TDX has requirement to force the DMA API (which then forces
    swiotlb), but without relying on the host.
    
    Enable virtio restricted memory access for TDX.
    
    Signed-off-by: Andi Kleen <ak@linux.intel.com>
    Signed-off-by: Alexander Shishkin <alexander.shishkin@linux.intel.com>

--- arch/x86/coco/tdx/tdx.c
+++ arch/x86/coco/tdx/tdx.c
@@ -5,7 +5,9 @@
 #define pr_fmt(fmt)     "tdx: " fmt
 
 #include <linux/cpufeature.h>
+#include <linux/io.h>
 #include <linux/random.h>
+#include <linux/virtio_anchor.h>
 #include <asm/coco.h>
 #include <asm/tdx.h>
 #include <asm/vmx.h>
@@ -779,6 +781,9 @@ void __init tdx_early_init(void)
 	 * just in case it's some kind of host attack.
 	 */
 	panic_on_oops = 1;
+
+	/* Set restricted memory access for virtio. */
+	virtio_set_mem_acc_cb(virtio_require_restricted_mem_acc);
 
 	cc_set_vendor(CC_VENDOR_INTEL);
 	cc_mask = get_cc_mask();
