639037b89f5af13a26bce6510c4077f4902af310
Andi Kleen <ak@linux.intel.com>
2021-06-18 19:11:11 -0700

    x86/tdx: Enable panic_on_oops by default in TDX
    For a confidential guest it seems safer to always panic on oopses.
    Some kind of attack from the host might result in a oops, and
    the oops recovery path can leave kernel subsystems in various
    undefined states, which could be used by the attacker.
    
    This can be still overridden after boot using the sysctl.
    
    Signed-off-by: Andi Kleen <ak@linux.intel.com>
    Signed-off-by: Kuppuswamy Sathyanarayanan <sathyanarayanan.kuppuswamy@linux.intel.com>

--- arch/x86/coco/tdx/tdx.c
+++ arch/x86/coco/tdx/tdx.c
@@ -765,6 +765,12 @@ void __init tdx_early_init(void)
 	 */
 	setup_force_cpu_cap(X86_FEATURE_TSC_RELIABLE);
 
+	/*
+	 * Make sure there is a panic if something goes wrong,
+	 * just in case it's some kind of host attack.
+	 */
+	panic_on_oops = 1;
+
 	cc_set_vendor(CC_VENDOR_INTEL);
 	cc_mask = get_cc_mask();
 	cc_set_mask(cc_mask);
