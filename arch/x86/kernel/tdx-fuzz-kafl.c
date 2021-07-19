3c00595344ddec1fb8ff6d9023bdbf63a72698b6
Steffen Schulz <steffen.schulz@intel.com>
2021-07-19 09:48:27 -0700

    dump payload mode

--- arch/x86/kernel/tdx-fuzz-kafl.c
+++ arch/x86/kernel/tdx-fuzz-kafl.c
@@ -22,14 +22,20 @@
 
 static bool agent_initialized = false;
 static bool agent_enabled = false;
+static agent_config_t agent_config = {0};
 
 static uint8_t payload_buffer[PAYLOAD_SIZE] __attribute__((aligned(4096)));
+static uint8_t observed_payload_buffer[PAYLOAD_SIZE] __attribute__((aligned(4096)));
 static uint32_t location_stats[TDX_FUZZ_MAX];
-	
+
 static u64 *ve_buf;
 static u32 ve_num;
 static u32 ve_pos;
 static u32 ve_mis;
+
+static u64 *ob_buf;
+static u32 ob_num;
+static u32 ob_pos;
 
 void kafl_raise_panic(void) {
 	kAFL_hypercall(HYPERCALL_KAFL_PANIC, 0);
@@ -51,11 +57,23 @@ void kafl_agent_setrange(void)
 	kAFL_hypercall(HYPERCALL_KAFL_RANGE_SUBMIT, (uintptr_t)ranges);
 }
 
-void kafl_raise_abort(char *msg)
+void kafl_agent_abort(char *msg)
 {
 	hprintf(msg);
 	kAFL_hypercall(HYPERCALL_KAFL_USER_ABORT, 0);
 	BUG();
+}
+
+static
+void kafl_dump_observed_payload(char *filename, uint8_t *buf, uint32_t buflen)
+{
+	kafl_dump_file_t f;
+
+	f.file_name_str_ptr = (uint64_t)filename;
+	f.data_ptr = (uint64_t)buf;
+	f.bytes = buflen;
+	pr_info("Submit payload dump %p\n", &f);
+	kAFL_hypercall(HYPERCALL_KAFL_DUMP_FILE, (uint64_t)&f);
 }
 
 void kafl_agent_init(void)
@@ -63,10 +81,8 @@ void kafl_agent_init(void)
 	kAFL_payload* payload = (kAFL_payload*)payload_buffer;
 
 	if (agent_initialized) {
-		kafl_raise_abort("Warning: Agent was already initialized!\n");
+		kafl_agent_abort("Warning: Agent was already initialized!\n");
 	}
-
-	agent_enabled = true;
 
 	hprintf("[*] Initialize kAFL Agent\n");
 
@@ -82,10 +98,19 @@ void kafl_agent_init(void)
 #endif
 
 	/* ensure that the virtual memory is *really* present in physical memory... */
+	memset(observed_payload_buffer, 0xff, PAYLOAD_SIZE);
 	memset(payload, 0xff, PAYLOAD_SIZE);
 
 	hprintf("Submitting payload buffer address to hypervisor (%lx)\n", payload);
 	kAFL_hypercall(HYPERCALL_KAFL_GET_PAYLOAD, (uint64_t)payload);
+
+	kAFL_hypercall(HYPERCALL_KAFL_SET_AGENT_CONFIG, (uintptr_t)&agent_config);
+	if (agent_config.dump_payloads) {
+		ob_buf = (u64*)observed_payload_buffer;
+		ob_num = sizeof(observed_payload_buffer)/sizeof(u64);
+		ob_pos = 0;
+		hprintf("Enabled dump payload (max_entries=%u)\n", ob_num);
+	}
 
 	//hprintf("Submitting current CR3 value to hypervisor...\n");
 	//kAFL_hypercall(HYPERCALL_KAFL_SUBMIT_CR3, 0);
@@ -113,6 +138,8 @@ void kafl_agent_done(void)
 {
 	unsigned i;
 
+	hprintf("agent done!\n");
+
 	if (!agent_initialized)
 		return;
 
@@ -125,6 +152,12 @@ void kafl_agent_done(void)
 		}
 	}
 
+	// Dump observed values
+	if (agent_config.dump_payloads) {
+		hprintf("Dumping observed input...\n");
+		kafl_dump_observed_payload("payload.txt", (uint8_t*)ob_buf, ob_pos*sizeof(ob_buf[0]));
+	}
+
 	// Stops tracing and restore the snapshot
 	// Non-zero argument triggers stream_expand mutation in kAFL
 	kAFL_hypercall(HYPERCALL_KAFL_RELEASE, ve_mis*sizeof(ve_buf[0]));
@@ -133,11 +166,11 @@ void kafl_agent_done(void)
 void kafl_agent_stop(void)
 {
 	if (!agent_enabled) {
-		kafl_raise_abort("Attempt to finish kAFL run but not yet enabled\n");
+		kafl_agent_abort("Attempt to finish kAFL run but not yet enabled\n");
 	}
 
 	if (!agent_initialized) {
-		kafl_raise_abort("Attempt to finish kAFL run but never initialized\n");
+		kafl_agent_abort("Attempt to finish kAFL run but never initialized\n");
 	}
 
 	kafl_agent_done();
@@ -152,8 +185,23 @@ u64 kafl_fuzz_var(u64 var)
 	}
 	else {
 		ve_mis++;
-		kafl_agent_done(); // stop at end of fuzz input?
+		// stop at end of fuzz input, unless in dump mode
+		if (!agent_config.dump_payloads)
+			kafl_agent_done();
 	}
+
+	if (agent_config.dump_payloads) {
+		// record input seen so far
+		// execution may be (have been) partly driven by fuzzer
+		if (ob_pos < ob_num) {
+			//hprintf("dump_payload: ob_buf[%u] = %08lx\n", ob_pos, var);
+			ob_buf[ob_pos++] = var;
+		} else {
+			hprintf("Error: insufficient space in dump_payload");
+			kafl_agent_done();
+		}
+	}
+
 	return var;
 }
 
@@ -196,6 +244,7 @@ void tdx_fuzz_event(enum tdx_fuzz_event e)
 			return kafl_raise_kasan();
 		case TDX_FUZZ_DONE:
 			//return kafl_agent_stop();
+			return kafl_agent_done();
 		case TDX_FUZZ_HALT:
 		case TDX_FUZZ_REBOOT:
 		case TDX_FUZZ_SAFE_HALT:
@@ -214,6 +263,7 @@ void tdx_fuzz_event(enum tdx_fuzz_event e)
 			}
 			break;
 		default:
-			return kafl_raise_abort("Unrecognized fuzz event.");
+			return kafl_agent_abort("Unrecognized fuzz event.");
+
 	}
 }
