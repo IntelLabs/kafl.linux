00dd7fb8e1cd60e0637386e03dc29f0c8f6ee9eb
Steffen Schulz <steffen.schulz@intel.com>
2021-08-29 14:48:36 -0700

    TDX: add addr + size info to tdg_fuzz() hook

--- arch/x86/kernel/tdx.c
+++ arch/x86/kernel/tdx.c
@@ -498,7 +498,7 @@ static bool tdx_read_msr_safe(unsigned int msr, u64 *val)
 		return false;
 
 	/* Should filter the MSRs to only fuzz host controlled */
-	*val = tdx_fuzz(out.r11, TDX_FUZZ_MSR_READ);
+	*val = tdx_fuzz(out.r11, msr, 8, TDX_FUZZ_MSR_READ);
 
 	return true;
 }
@@ -570,10 +570,10 @@ static bool tdx_handle_cpuid(struct pt_regs *regs)
 	 * EAX, EBX, ECX, EDX registers after CPUID instruction execution.
 	 * So copy the register contents back to pt_regs.
 	 */
-	regs->ax = tdx_fuzz(out.r12, TDX_FUZZ_CPUID1);
-	regs->bx = tdx_fuzz(out.r13, TDX_FUZZ_CPUID2);
-	regs->cx = tdx_fuzz(out.r14, TDX_FUZZ_CPUID3);
-	regs->dx = tdx_fuzz(out.r15, TDX_FUZZ_CPUID4);
+	regs->ax = tdx_fuzz(out.r12, -1, 2, TDX_FUZZ_CPUID1);
+	regs->bx = tdx_fuzz(out.r13, -1, 2, TDX_FUZZ_CPUID2);
+	regs->cx = tdx_fuzz(out.r14, -1, 2, TDX_FUZZ_CPUID3);
+	regs->dx = tdx_fuzz(out.r15, -1, 2, TDX_FUZZ_CPUID4);
 
 	return true;
 }
@@ -616,7 +616,7 @@ static bool tdx_handle_io(struct pt_regs *regs, u32 exit_qual)
 					   &outh);
 		regs->ax &= ~mask;
 		regs->ax |= tdx_fuzz(ret || tdx_fuzz_err(TDX_FUZZ_PORT_IN_ERR) ?
-				UINT_MAX : outh.r11, TDX_FUZZ_PORT_IN)
+				UINT_MAX : outh.r11, port, size, TDX_FUZZ_PORT_IN)
 			& mask;
 	} else {
 		ret = _tdx_hypercall(EXIT_REASON_IO_INSTRUCTION,
@@ -635,9 +635,7 @@ static unsigned long tdx_mmio(int size, bool write, unsigned long addr,
 
 	err = _trace_tdx_hypercall(EXIT_REASON_EPT_VIOLATION, size, write,
 				   addr, *val, &out);
-
-	*val = tdx_fuzz(out.r11, TDX_FUZZ_MMIO_READ);
-
+	*val = tdx_fuzz(out.r11, addr, size, TDX_FUZZ_MMIO_READ);
 	return err;
 }
 
