3280c2a036f8dbc7918aa25a548cacedb32f02c2
Sebastian Österlund <sebastian.osterlund@intel.com>
2021-10-20 08:25:37 -0700

    Revert "kAFL/ virtio: add some wrappers for reading shared memory"
    This reverts commit 6de7296157e182379a9667f1ebecf6b46b6da64c.
    
    (Will keep this in separate fuzz-13-virtio branch)

--- drivers/virtio/virtio_ring.c
+++ drivers/virtio/virtio_ring.c
@@ -694,6 +694,7 @@ static void detach_buf_split(struct vring_virtqueue *vq, unsigned int head,
 			     void **ctx)
 {
 	unsigned int i, j;
+	__virtio16 nextflag = cpu_to_virtio16(vq->vq.vdev, VRING_DESC_F_NEXT);
 
 	/* Clear data ptr. */
 	vq->split.desc_state[head].data = NULL;
@@ -701,7 +702,7 @@ static void detach_buf_split(struct vring_virtqueue *vq, unsigned int head,
 	/* Put back on free list: unmap first-level descriptors and find end */
 	i = head;
 
-	while (virtio16_to_cpu(vq->vq.vdev, vq->split.vring.desc[i].flags) & VRING_DESC_F_NEXT) {
+	while (vq->split.vring.desc[i].flags & nextflag) {
 		vring_unmap_one_split(vq, i);
 		i = vq->split.desc_extra[i].next;
 		vq->vq.num_free++;
