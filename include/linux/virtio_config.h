589ba6a32b6473bd82a58523b2caac34ce586fa4
Daniel Sneddon <daniel.sneddon@linux.intel.com>
2023-02-28 20:36:45 -0700

    Add fuzzing support for virtio subsystem and its drivers
    This adds the following functionality:
    1) an ability to fuzz the virtio drivers init/probe code via generic
    TDX_FUZZ_HARNESS_DOINTCALLS_VIRTIO harness and virtio-blk device specific
    TDX_FUZZ_HARNESS_VRTIO_BLK_PROBE harness.
    2) an ability to fuzz the virtio shared memory reds (residing in DMA) via
    TDX_FUZZ_KAFL_VIRTIO config option.  This is done by injecting inputs to
    functions like _virtioXX_to_cpu and virtqueue_get_buf

--- include/linux/virtio_config.h
+++ include/linux/virtio_config.h
@@ -341,7 +341,12 @@ static inline bool virtio_is_little_endian(struct virtio_device *vdev)
 /* Memory accessors */
 static inline u16 virtio16_to_cpu(struct virtio_device *vdev, __virtio16 val)
 {
+#ifdef CONFIG_TDX_FUZZ_KAFL_VIRTIO_TO_CPU
+	u16 ret = __virtio16_to_cpu(virtio_is_little_endian(vdev), val);
+	return tdx_fuzz_virtio_cache_get_u16(vdev, ret);
+#else
 	return __virtio16_to_cpu(virtio_is_little_endian(vdev), val);
+#endif
 }
 
 static inline __virtio16 cpu_to_virtio16(struct virtio_device *vdev, u16 val)
@@ -351,7 +356,12 @@ static inline __virtio16 cpu_to_virtio16(struct virtio_device *vdev, u16 val)
 
 static inline u32 virtio32_to_cpu(struct virtio_device *vdev, __virtio32 val)
 {
+#ifdef CONFIG_TDX_FUZZ_KAFL_VIRTIO_TO_CPU
+	u32 ret = __virtio32_to_cpu(virtio_is_little_endian(vdev), val);
+	return tdx_fuzz_virtio_cache_get_u32(vdev, ret);
+#else
 	return __virtio32_to_cpu(virtio_is_little_endian(vdev), val);
+#endif
 }
 
 static inline __virtio32 cpu_to_virtio32(struct virtio_device *vdev, u32 val)
@@ -361,7 +371,12 @@ static inline __virtio32 cpu_to_virtio32(struct virtio_device *vdev, u32 val)
 
 static inline u64 virtio64_to_cpu(struct virtio_device *vdev, __virtio64 val)
 {
+#ifdef CONFIG_TDX_FUZZ_KAFL_VIRTIO_TO_CPU
+	u64 ret = __virtio64_to_cpu(virtio_is_little_endian(vdev), val);
+	return tdx_fuzz_virtio_cache_get_u64(vdev, ret);
+#else
 	return __virtio64_to_cpu(virtio_is_little_endian(vdev), val);
+#endif
 }
 
 static inline __virtio64 cpu_to_virtio64(struct virtio_device *vdev, u64 val)
